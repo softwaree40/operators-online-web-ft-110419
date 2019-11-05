b7449b0317dec35038c1c2b7ab0bd1ab45aa7a00
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-05 00:37:19 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,28 @@
-def unsafe?(speed)
 
+require 'pry'
+def unsafe?(speed)
+    if speed > 60 
+      return true
+    elsif speed < 40
+       true
+    else 
+      speed <= 50
+      return false 
+    
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+   if speed > 60
+      return true
+   elsif speed < 40
+      return true
+   else speed <= 50
+      return false 
+      
+  end
 end
 	
 
